233f13da56a4ec048d2fe18d637046f8d99da25e
sonalgupta276 <sonalgupta276@gmail.com>
2020-09-23 15:41:23 +0530

    Solve chunk-array-in-groups

---
+++ js-exercises/chunk-array-in-groups/chunkArrayInGroups.js
@@ -0,0 +1,12 @@
+function chunkArrayInGroups(array, chunkSize) {
+  const result = [];
+  for (let i = 0; i < array.length; i += chunkSize) {
+    const chunk = array.slice(i, i + chunkSize);
+    result.push(chunk);
+  }
+  return result;
+}
+
+export {
+  chunkArrayInGroups,
+};
